14b35741dbae63e3bf92c347eb7b2ad75ba0454a
Srvizz <info@srvizz.com>
2017-06-05 21:26:21 +0100

    1
    1

--- nodejs1.js
+++ nodejs1.js
@@ -1,6 +1,17 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
-})
+var LayerAPI = require('layer-api');
+ 
+// Initialize by providing your Layer credentials 
+var layer = new LayerAPI({
+  token: API_TOKEN,
+  appId: APP_ID
+});
+ 
+// Create a Conversation 
+layer.conversations.create({participants: ['abcd']}, function(err, res) {
+  var cid = res.body.id;
+ 
+  // Send a Message 
+  layer.messages.sendTextFromUser(cid, 'abcd', 'Hello, World!', function(err, res) {
+    console.log(err || res.body);
+  });
+});
