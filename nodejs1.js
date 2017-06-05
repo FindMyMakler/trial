54dce4aa0d66b61fd33b69523e835d9dbf77ef67
Srvizz <info@srvizz.com>
2017-06-05 21:31:29 +0100

    1
    1

--- nodejs1.js
+++ nodejs1.js
@@ -2,8 +2,8 @@ var LayerAPI = require('layer-api');
  
 // Initialize by providing your Layer credentials 
 var layer = new LayerAPI({
-  token: API_TOKEN,
-  appId: APP_ID
+  token: "vnsELsfceQQBFo7jshxzbBOKmWOLxE4J6dNBR8V0q4XpfoZu",
+	appId: "layer:///apps/staging/2c96c436-44b0-11e7-9f2f-b79ffcf05b7b"
 });
  
 // Create a Conversation 
